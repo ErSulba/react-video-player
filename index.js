5d980e66005615b277dd13bd64664dddbce8bb8b
Alejandro Sulbaran <sulbaran.kun@gmail.com>
2018-01-20 17:15:36 -0400

    CSS added

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-import data from './src/api.json';
-//Here we register all the components for the app
-// import Playlist from './src/playlist/components/playlist';
-import Content from './src/playlist/components/content';
-
-const app = document.getElementById('app');
-render( <Content data={data} /> , app );
--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,73 +1,17 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import Media from './media.js';
-import './playlist.css'
-import Play from '../../icons/components/play'
-import Volume from '../../icons/components/volume';
-import Pause from '../../icons/components/pause';
-import FullScreen from '../../icons/components/full-screen';
+import './playlist.css';
 
-// class Playlist extends Component {
-//   render() {
-//     const playlist = this.props.data.categories[0].playlist
-//     console.log(this.props.data);
-//     return (
-//       <div className="Playlist">
-//         {
-//           playlist.map((item) => {
-//             return <Media {...item} key={item.id} />
-//           })
-//         }
-//       </div>
-//     )
-//   }
-// }
-
-class Playlist extends Component{
-	render() {
-		const {title, description, list} = this.props // --- playlist
-		return (
-			<div className = "Playlist">
-				<Play
-				size= {20}
-				color= "red"
-				/>
-				<Volume
-				size= {20}
-				color= "blue"
-				/>
-				<Pause
-				size= {20}
-				color= "green"
-				/>
-				<FullScreen
-					size= {20}
-					color= "yellow"
-				/>
-
-				<h3 className = "Playlist-description">{description}</h3>
-				<h1 className = "Playlist-title">{title}</h1>
-				<div className = "Playlist-scroller">
-					{
-						list.map( item => {
-							return <Media
-											title  = {item.title}
-											author = {item.author}
-											cover  = {item.cover}
-											type   = {item.type}
-											key    = {item.id}
-										/>
-						})
-					}
-				</div>
-			</div>
-		)
-	}
+function Playlist(props) {
+  return (
+    <div className="Playlist">
+      {
+        props.playlist.map((item) => {
+          return <Media {...item} key={item.id} />
+        })
+      }
+    </div>
+  )
 }
 
-Playlist.propTypes = {
-  title: PropTypes.string,
-  description: PropTypes.string,
-  list: PropTypes.array,
-}
 export default Playlist;
